a482637c72646226a3d7bd6f4a19ad36e54ff0ea
Julio-SC <jhver2001@gmail.com>
2024-09-25 12:28:46 -0700

    ex 05 completed

--- ex05.js
+++ ex05.js
@@ -1,3 +1,10 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const array = [3,5,7,9,13];
+
+const even = (element) => {
+    return element % 2 === 0;
+}
+
+console.log(array.some(even));
